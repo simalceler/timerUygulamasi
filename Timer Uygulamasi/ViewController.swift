f8f5a6a5025e4c48d848ac00e61d2e3452d340f6
simalceler <celersimal@gmail.com>
2023-05-23 12:12:27 +0300

    git add.

--- Timer Uygulamasi/ViewController.swift
+++ Timer Uygulamasi/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var sayici:Timer?
+    var sayac1:Int = 1
+    var sayac2:Int = 5
+    
+    @IBOutlet weak var ciktiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ciktiLabel.text = ""
+        
     }
 
-
+    @IBAction func basla(_ sender: Any) {
+        
+        sayici = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(geriSay), userInfo: nil, repeats: true)
+    }
+    
+    @objc func ileriSay(){
+        
+        ciktiLabel.text = String(sayac1)
+        sayac1 += 1
+        
+        if sayac1 > 6 {
+            sayici?.invalidate()
+            ciktiLabel.text = "Süre Bitti"
+            sayac1 = 1
+        }
+        
+    }
+    
+    @objc func geriSay(){
+        
+        ciktiLabel.text = String(sayac2)
+        sayac2 -= 1
+        
+        if sayac2 < 0 {
+            sayici?.invalidate()
+            ciktiLabel.text = "Süre Bitti"
+            sayac2 = 5
+        }
+        
+    }
+    
 }
 
